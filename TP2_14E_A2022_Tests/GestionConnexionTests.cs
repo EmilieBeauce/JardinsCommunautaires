2c9349ba1e7b3238b6230bd56132eb7e45e68430
vikteaur <viktor.vanic.protic@gmail.com>
2023-05-10 10:42:50 -0400

    ijdsadka

--- TP2_14E_A2022_Tests/GestionConnexionTests.cs
+++ TP2_14E_A2022_Tests/GestionConnexionTests.cs
@@ -72,7 +72,7 @@ namespace TP2_14E_A2022.Data.Gestions.Tests
             
             var result = mock.Object.CreerCompteGestionnaire("John", "Doe", "jd@courriel.com", "password");
 
-            Assert.IsNotNull(result);
+            Assert.IsNull(result);
             Assert.AreEqual(result.Prenom, "John");
             Assert.AreEqual(result.Nom, "Doe");
             Assert.AreEqual(result.Courriel, "jd@courriel.com");
@@ -90,7 +90,7 @@ namespace TP2_14E_A2022.Data.Gestions.Tests
 
             Gestionnaire nouveauGestionnaire = gestion.CreerCompteGestionnaire(prenom, nom, courriel, motDePasse);
 
-            Assert.IsNotNull(nouveauGestionnaire);
+            Assert.IsNull(nouveauGestionnaire);
             Assert.AreEqual(prenom, nouveauGestionnaire.Prenom);
             Assert.AreEqual(nom, nouveauGestionnaire.Nom);
             Assert.AreEqual(courriel, nouveauGestionnaire.Courriel);
